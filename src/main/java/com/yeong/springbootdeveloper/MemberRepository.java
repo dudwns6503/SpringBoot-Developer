553cbb6a2acef03979a8acc77b5fb864e7f22ef0
yeongjun <dudwns650395@gmail.com>
2024-07-30 23:37:54 +0900

    Repository 테스트 작성
    다양한 JpaRepository의 메서드를 사용해 보았다.

--- src/main/java/com/yeong/springbootdeveloper/MemberRepository.java
+++ src/main/java/com/yeong/springbootdeveloper/MemberRepository.java
@@ -3,6 +3,10 @@ package com.yeong.springbootdeveloper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByName(String name);
 }
