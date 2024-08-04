502add94287b9a4665e561a323f937408371dfad
yeongjun <dudwns650395@gmail.com>
2024-08-04 22:41:43 +0900

    Spring Security를 통한 회원가입 + 로그인 구현

---
+++ src/main/java/com/yeong/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.yeong.springbootdeveloper.service;
+
+import com.yeong.springbootdeveloper.domain.User;
+import com.yeong.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
---
+++ src/main/java/com/yeong/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package com.yeong.springbootdeveloper.service;
+
+import com.yeong.springbootdeveloper.domain.User;
+import com.yeong.springbootdeveloper.dto.AddUserRequest;
+import com.yeong.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
