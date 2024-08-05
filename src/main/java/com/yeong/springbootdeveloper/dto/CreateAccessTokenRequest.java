16e4437535884e384d8b98d701bf71579870533f
yeongjun Kim <dudwns650395@gmail.com>
2024-08-05 17:53:12 +0900

    JWT AccessToken 및 Refresh Token 구현

---
+++ src/main/java/com/yeong/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,11 @@
+package com.yeong.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+
+    private String refreshToken;
+}
