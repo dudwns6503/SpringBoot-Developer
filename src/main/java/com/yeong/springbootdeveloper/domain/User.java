43169ed4ec622d324789126780e0cc074d89e522
yeongjun Kim <dudwns650395@gmail.com>
2024-08-08 17:35:41 +0900

    OAuth 관련 클래스 생성(미완)

--- src/main/java/com/yeong/springbootdeveloper/domain/User.java
+++ src/main/java/com/yeong/springbootdeveloper/domain/User.java
@@ -1,7 +1,6 @@
 package com.yeong.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,10 +32,20 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
+    }
+
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 
     @Override
